c7e32db1dc3fb5f2ac3af8c17b2fdb9c0db088af
acer <acer@192.168.0.106>
2022-06-29 12:47:05 +0530

    Ternery Operator

---
+++ SvkpCG/src/Ternery.java
@@ -0,0 +1,10 @@
+
+public class Ternery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 11;
+		x = (10 == x) ? 1 : 0; 
+		System.out.println(x);
+	}
+}
