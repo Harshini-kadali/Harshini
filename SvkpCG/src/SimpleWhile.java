635600e396642e87f64bacb60670e997013d6a93
acer <acer@192.168.0.106>
2022-06-29 14:42:19 +0530

    Smiple while

---
+++ SvkpCG/src/SimpleWhile.java
@@ -0,0 +1,14 @@
+
+public class SimpleWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=99;
+		while(i<=100)
+		{
+		System.out.println(i);
+		i++;
+		}
+	}
+
+}
