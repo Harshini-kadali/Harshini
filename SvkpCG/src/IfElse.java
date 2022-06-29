753b4a89708984430204611c0ad8bfbaeeafef14
acer <acer@192.168.0.106>
2022-06-29 12:32:54 +0530

    If Else

---
+++ SvkpCG/src/IfElse.java
@@ -0,0 +1,17 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10, y = 11;
+
+		if(x >= y)
+		{
+		System.out.println("true");
+		}
+		else
+		{
+		System.out.println("false");
+	}
+	}
+}
