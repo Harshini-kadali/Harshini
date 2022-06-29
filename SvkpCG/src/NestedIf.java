39c989b5da1ba8494551b5a1856b19a0088c8d34
acer <acer@192.168.0.106>
2022-06-29 12:43:19 +0530

    Nested If

---
+++ SvkpCG/src/NestedIf.java
@@ -0,0 +1,16 @@
+
+public class NestedIf {
+
+	public static void main(String[] args) 
+	{
+		int x = 10; int y = 11;
+		if(!(x < y) || (x == y))
+		{
+		System.out.println("Condition is TRUE");
+		}
+		else
+		{
+		System.out.println("Condition is FALSE");
+		}
+		}
+	}
