d4853382d339d44152cadf7bf194b6f235c94131
Harshi <Harshi@harshini>
2022-07-14 14:36:39 +0530

    Swapping of two numbers

---
+++ SvkpCG/src/SvkpCG/src/Swap.java
@@ -0,0 +1,20 @@
+
+public class Swap {
+
+	public static void main(String[] args) {
+    int x=20,y=70;
+    System.out.println("Before swap:");
+    System.out.println("x="+x);
+    System.out.println("y="+y);
+    
+    int temp=x;
+    temp=x;
+    x=y;
+    y=temp;
+    System.out.println("Before swap");
+    System.out.println("x="+x);
+    System.out.println("y="+y);
+	
+}
+
+}
