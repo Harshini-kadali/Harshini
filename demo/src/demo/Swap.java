125f21d34cac27bf737aa117439a31b80fd19322
Harshi <Harshi@harshini.bbrouter>
2022-07-15 13:11:20 +0530

    Swapping of two numbers.

---
+++ demo/src/demo/Swap.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Swap {
+
+	public static void main(String[] args) {
+     int x=30,y=70;
+     System.out.println("Before swap:");
+     System.out.println(" x:"+x);
+     System.out.println(" y:"+y);
+     int temp=x;
+     x=y;
+     y=temp;
+     System.out.println("After Swap:" );
+     System.out.println(" x:"+x);
+     System.out.println(" y:"+y);
+     
+	}
+
+}
