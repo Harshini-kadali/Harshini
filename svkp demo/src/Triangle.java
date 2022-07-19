403fe9822bcccd2cb0becee6b1bf07c803f8293b
Harshi <Harshi@harshini>
2022-07-19 10:03:45 +0530

    Number triangle

---
+++ svkp demo/src/Triangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Triangle {
+public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("How many rows do you want to print:");
+		int rows=sc.nextInt();
+		System.out.println("here is your pattern..");
+		for(int i=1;i<=rows;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i+" ");
+			}
+			System.out.println();
+				}
+		sc.close();
+			}
+	}
+
